c8d39bf866031dc184b25be62816b645b5b48762
rhavock <rhavock@outlook.com>
2015-10-07 21:17:59 -0300

    Menu

--- R2L2.Presentation/App_Start/RouteConfig.cs
+++ R2L2.Presentation/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace R2L2.Presentation
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
